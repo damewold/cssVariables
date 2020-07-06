d4c613788528b528f295fb78f2f21a488b4b02c4
Denis Woldemariam <damewold@gmail.com>
2020-07-06 00:32:43 -0500

    add the function handleChange

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ document.addEventListener('DOMContentLoaded', function(e){
    handleChange()
 })
 
+//use queryselectorall to access all the inputs you want to manipulate 
+
+const allInputs = document.querySelectorAll('.controls input')
+
+
 function handleChange(){
     console.log('in handleChange')
+    //grab the database onject that contains the value px 
+    const suffix = this.database.sizing ||'';
+    //using the setproperty method of style to manipulate the ccs properties of the inputs
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
 }
+
+//add eventlisteners to the inputs so that user can interact with them
+
