aa0e0d0b9a850edfc2e75f57a22552dcf5591d68
Denis Woldemariam <damewold@gmail.com>
2020-07-06 00:04:57 -0500

    inserted function that will check for document loading

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+console.log('hello');
+
+//in order to access the html building blocks you have let the page load first
+//once aware of that you can access and manupilate to your liking 
+document.addEventListener('DOMContentLoaded', function(e){
+    console.log('loaded');
+   handleChange()
+})
+
+function handleChange(){
+    console.log('in handleChange')
+}
