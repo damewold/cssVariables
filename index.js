5776d9f2a9b5177a09b78007575ad39c8f4f6dd6
Denis Woldemariam <damewold@gmail.com>
2020-07-06 01:25:22 -0500

    fixed the dataset error

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ console.log('hello');
 //once aware of that you can access and manupilate to your liking 
 document.addEventListener('DOMContentLoaded', function(e){
     console.log('loaded');
-   handleChange()
-})
+  //use queryselectorall to access all the inputs you want to manipulate 
 
-//use queryselectorall to access all the inputs you want to manipulate 
-
-const allInputs = document.querySelectorAll('.controls input')
+const inputs = document.querySelectorAll('.controls input')
 
 
 function handleChange(){
     console.log('in handleChange')
-    //grab the database onject that contains the value px 
-    const suffix = this.database.sizing ||'';
+    console.log(this.name)
+    //grab the dataset onject that contains the value px 
+    const suffix =this.dataset.sizing||'';
+    console.log(this.dataset)
     //using the setproperty method of style to manipulate the ccs properties of the inputs
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 
 }
 
 //add eventlisteners to the inputs so that user can interact with them
+console.log(inputs)
+inputs.forEach(input => input.addEventListener('change', handleChange))
+inputs.forEach(input => input.addEventListener('mousemove', handleChange))
+})
+
 
